4cb6b414853a463e7c075790f7d68a4e75f9affe
Sergi Gracia <sergigram@gmail.com>
2015-12-03 12:27:26 +0100

    Update AppDelegate to manage 3D touch

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -12,6 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        // Fuuuu
         return true
     }
 }
