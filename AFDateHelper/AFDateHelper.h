f5f49c4cf25a7c32132dadd2ba5bed29e6ab9725
Patrick <patrick@helmsdeep.at>
2016-04-07 07:51:26 +0200

    added shared framework scheme

---
+++ AFDateHelper/AFDateHelper.h
@@ -0,0 +1,19 @@
+//
+//  AFDateHelper.h
+//  AFDateHelper
+//
+//  Created by Patrick Steiner on 07.04.16.
+//  Copyright © 2016 All Forces. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AFDateHelper.
+FOUNDATION_EXPORT double AFDateHelperVersionNumber;
+
+//! Project version string for AFDateHelper.
+FOUNDATION_EXPORT const unsigned char AFDateHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFDateHelper/PublicHeader.h>
+
+
