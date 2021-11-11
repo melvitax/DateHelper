63b9f982dce0bbeb4e6bb795bca5b451432d69f7
Melvin Rivera <melvitax@me.com>
2021-11-11 11:42:45 -0500

    Updated to a proper package manager directory structure

--- Package.swift
+++ Package.swift
@@ -1,12 +1,5 @@
-// swift-tools-version:5.3
-//
-//  AFDateHelper.swift
-//  https://github.com/melvitax/DateHelper
-//  Version 4.5.2
-//
-//  Created by David Velarde on 6/7/19.
-//  Copyright (c) 2019. All rights reserved.
-//
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -19,16 +12,19 @@ let package = Package(
         .watchOS(.v4)
     ],
     products: [
-        .library(name: "DateHelper", targets: ["DateHelper"])
+        .library(
+            name: "DateHelper",
+            targets: ["DateHelper"]),
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DateHelper",
-            path: "Sources",
-            exclude: [
-                "Info.plist"
-            ]
-        )
+            dependencies: []),
+        .testTarget(
+            name: "DateHelperTests",
+            dependencies: ["DateHelper"]),
     ],
     swiftLanguageVersions: [.v5]
 )
