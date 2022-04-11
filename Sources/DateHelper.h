48b6ba565a184125ddf814dababa4fa880cf79da
Jose Gil <jose.gil@scrm.lidl>
2022-04-11 17:20:10 +0200

    Restore Carthage support

---
+++ Sources/DateHelper.h
@@ -0,0 +1,17 @@
+//
+//  DateHelper.swift
+//  Version: 5.0.1
+//  https://github.com/melvitax/DateHelper
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DateHelper IOS.
+FOUNDATION_EXPORT double DateHelperVersionNumber;
+
+//! Project version string for DateHelper IOS.
+FOUNDATION_EXPORT const unsigned char DateHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DateHelper_IOS/PublicHeader.h>
+
+
